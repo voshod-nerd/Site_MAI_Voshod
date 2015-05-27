bd2edd080d32012bf432f6bf29ade98ab8ba73d1
Sokolov Oleg <voshod-nerd@hotmail.com>
2015-05-27 09:45:16 +0600

    2th commit

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -101,30 +101,7 @@ class SiteController extends Controller
         return $this->render('about');
     }
 	
- /////////////////////////////////////////   
-	public function actionAddcontent() 
-{
 
-
- $model = new Posts();
-       $this->layout='//adminka';
-       
-        if ($model->load(Yii::$app->request->post())) 
-        {
-        $form = Yii::$app->request->post('Posts');
-       
-        //$model->saved($form['text']);
-        $model->Save();
-        }
-
-               $query = Posts::find();
-              
-              
-               $listpost = $query->orderBy('dateadd')->all();
-              
-               return  $this->render('addcontent',['model'=>$model,'listpost' => $listpost]);
-		
-	}
 /////////////////////////////////////////////
 
 public function actionAdd()
@@ -209,15 +186,58 @@ return $this->render('showlibrary', ['model' => $model,'data'=>$data]);
     }
 
 
+ //   Добавление материалов сайта
+    public function actionAddcontent() 
+{
+
+
+ $model = new Posts();
+       $this->layout='//adminka';
+       
+        if ($model->load(Yii::$app->request->post())) 
+        {
+        $form = Yii::$app->request->post('Posts');
+       
+        //$model->saved($form['text']);
+        $model->Save();
+        }
+
+               $query = Posts::find();
+              
+              
+               $listpost = $query->orderBy('dateadd')->all();
+              
+               return  $this->render('addcontent',['model'=>$model,'listpost' => $listpost]);
+        
+    }
+
+
 // Добавление редактирование расписания 
 
 public function actionAddraspisanie()
 {
 
- $model = new Raspisanie();   
- $this->layout='//adminka';
+ //$model = new Raspisanie();   
+ //$this->layout='//adminka';
 
- return $this->render('addraspisanie',['model'=>$model]);
+ //return $this->render('addraspisanie',['model'=>$model]);
+
+
+ $model = new Raspisanie();
+       $this->layout='//adminka';
+       
+        if ($model->load(Yii::$app->request->post())) 
+        {
+        $form = Yii::$app->request->post('Raspisanie');
+                $model->Post();
+        }
+
+               $query = Raspisanie::find();
+
+               $dataRasp = $query->orderBy('id')->all();
+             return  $this->render('addraspisanie',['model'=>$model,'dataRasp' => $dataRasp]);
+
+
 
 
 
